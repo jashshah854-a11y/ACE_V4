0e1594f9c8203de4639282d3949e4dac78c9942f
Jash Shah <jashshah854@gmail.com>
2025-12-15 23:56:01 -0500

    fix: Remove space in ScrollArea variable name

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-const Scroll Area = React.forwardRef<
+const ScrollArea = React.forwardRef<
     HTMLDivElement,
     React.HTMLAttributes<HTMLDivElement>
 >(({ className, children, ...props }, ref) => (
